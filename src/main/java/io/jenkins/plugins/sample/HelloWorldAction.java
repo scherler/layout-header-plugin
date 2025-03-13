67eed6bed0af2f584978ff830881160c9ebf6cc6
Thorsten Scherler <scherler@gmail.com>
2025-03-13 12:10:31 +0100

    [master] initial commit
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

---
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -0,0 +1,31 @@
+package io.jenkins.plugins.sample;
+
+import hudson.model.Action;
+
+public class HelloWorldAction implements Action {
+    @Override
+    public String getIconFileName() {
+        return "document.png";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Greeting";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "greeting";
+    }
+
+    private String name;
+
+    public HelloWorldAction(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
